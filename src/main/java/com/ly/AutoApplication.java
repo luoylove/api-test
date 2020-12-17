05aa0b64540618800415880773312477e7daf447
luoyoujun <luoyoujun3712@dingtalk.com>
2020-12-17 13:17:56 +0800

    启动类

---
+++ src/main/java/com/ly/AutoApplication.java
@@ -0,0 +1,12 @@
+package com.ly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoApplication.class, args);
+    }
+}
